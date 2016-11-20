5dbb943a9601a45fc2747e246550cd91e01904eb
karol <karol.murray@sonalake.com>
2016-11-20 11:11:05 +0000

    Plot2 generating the Global Active Power over time

---
+++ plot2.R
@@ -0,0 +1,30 @@
+## Plot 2 - Showing Global Active Power over 2 days 
+
+library(dplyr)
+library(lubridate)
+
+source("get_the_data.R")
+
+createPlot2 <- function() {
+    hpc <- getTheData()
+    
+    ## Merging Date and Time columns together 
+    hpc <- mutate(hpc, time_stamp = dmy_hms(paste(hpc$Date, hpc$Time)))
+    
+    ## Filter down the data set to only be between dates 2007-02-01 and 2007-02-02
+    hpc <- filter(hpc, time_stamp >= ymd_hms("2007-02-01 00:00:00") & 
+                      time_stamp <= ymd_hms("2007-02-02 23:59:59"))
+    ## Adding a day column - may be not...
+    plot(hpc$time_stamp, 
+         hpc$Global_active_power,
+         type = "l", 
+         xlab = "", 
+         ylab = "Global Active Power (kilowatts)")
+    
+    dev.copy(png, file = "plot2.png", width = 480, height = 480, units = "px")
+    dev.off()
+}
+
+createPlot2()
+
+
